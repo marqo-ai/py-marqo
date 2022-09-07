f8e633e838b61abb6d86e91173058af9b2715c5d
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-07 10:23:58 +1000

    gutted py-marqo content

--- src/marqo/__init__.py
+++
@@ -1,9 +0,0 @@
-from marqo.client import Client
-from marqo.marqo_logging import logger
-from marqo.enums import SearchMethods
-
-
-def set_log_levels(level):
-    logger.setLevel(level)
-
-
--- src/marqo/version.py
+++
@@ -1,7 +0,0 @@
-__version__ = "0.1.0"
-
-
-def qualified_version() -> str:
-    """Get the qualified version of this module."""
-
-    return f"Marqo Python (v{__version__})"
