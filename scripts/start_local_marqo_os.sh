ef56d8611b36a4922fdf41f213839e7608a0b0f7
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-07 12:28:40 +1000

    start_local_marqo_os.sh changes

--- scripts/start_local_marqo_os.sh
+++ scripts/start_local_marqo_os.sh
@@ -15,10 +15,12 @@ cd "${MARQO_API_TESTS_ROOT}/temp/marqo" || exit
 git fetch
 git switch "$2"
 
+export LOCAL_OPENSEARCH_URL="https://localhost:9200"
+
 docker rm -f marqo-os &&
     docker run --name marqo-os -id -p 9200:9200 -p 9600:9600 -e "discovery.type=single-node" marqoai/marqo-os:0.0.2 &
 # wait for marqo-os to start
-until [[ $(curl -v --silent --insecure $OPENSEARCH_URL 2>&1 | grep Unauthorized) ]]; do
+until [[ $(curl -v --silent --insecure $LOCAL_OPENSEARCH_URL 2>&1 | grep Unauthorized) ]]; do
     sleep 0.1;
 done;
 
@@ -26,7 +28,7 @@ done;
 docker rm -f marqo
 DOCKER_BUILDKIT=1 docker build . -t marqo_docker_0 &&
     docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway \
-        -e "OPENSEARCH_URL=https://localhost:9200" marqo_docker_0 &
+        -e "OPENSEARCH_URL=$LOCAL_OPENSEARCH_URL" marqo_docker_0 &
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep marqo) ]]; do
     sleep 0.1;
