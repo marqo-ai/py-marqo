6e85b756daba4ef0a0fb14c59a4e41753ce488e6
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-07 17:20:18 +1000

    start_local_marqo_os.sh changes

--- tests/application_tests/test_start_stop.py
+++ tests/application_tests/test_start_stop.py
@@ -1,8 +1,9 @@
 import subprocess
+import time
 
 from tests import marqo_test
 from marqo import Client
-from marqo.errors import MarqoApiError
+from marqo.errors import MarqoApiError, BackendCommunicationError
 
 
 class TestStartStop(marqo_test.MarqoTestCase):
@@ -16,22 +17,36 @@ class TestStartStop(marqo_test.MarqoTestCase):
             pass
 
     def test_start_stop(self):
+        NUMBER_OF_TRIES = 30
+
         d1 = {"Title": "The colour of plants", "_id": "fact_1"}
         d2 = {"Title": "some frogs", "_id": "fact_2"}
         self.client.index(self.index_name_1).add_documents(documents=[d1, d2])
         search_res_0 = self.client.index(self.index_name_1).search(q="General nature facts")
         assert (search_res_0["hits"][0]["_id"] == "fact_1") or (search_res_0["hits"][0]["_id"] == "fact_2")
-        assert len(search_res_0) == 2
+        assert len(search_res_0["hits"]) == 2
 
         stop_marqo_res = subprocess.run(["docker", "stop", "marqo"], check=True, capture_output=True)
         assert "marqo" in str(stop_marqo_res.stdout)
 
         try:
             self.client.index(self.index_name_1).search(q="General nature facts")
-        except MarqoApiError as mqe:
-            print("ERROR CAUGHT:")
-            print(mqe)
+            raise AssertionError("Marqo is still accessible despite docker stopping!")
+        except BackendCommunicationError as mqe:
             pass
-        print("PASSED THE STOP ERROR")
+
         start_marqo_res = subprocess.run(["docker", "start", "marqo"], check=True, capture_output=True)
         assert "marqo" in str(start_marqo_res.stdout)
+
+        for i in range(NUMBER_OF_TRIES):
+            try:
+                self.client.index(self.index_name_1).search(q="General nature facts")
+                break
+            except BackendCommunicationError as mqe:
+                if i + 1 >= NUMBER_OF_TRIES:
+                    raise AssertionError("Timeout waiting for Marqo to restart!")
+                time.sleep(1)
+
+        search_res_1 = self.client.index(self.index_name_1).search(q="General nature facts")
+        assert search_res_1["hits"] == search_res_0["hits"]
+        assert (search_res_1["hits"][0]["_id"] == "fact_1") or (search_res_1["hits"][0]["_id"] == "fact_2")
