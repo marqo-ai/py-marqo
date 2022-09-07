d6211b28c40631e8a315b689a1a367e68b3f5868
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-07 17:26:42 +1000

    start_local_marqo_os.sh changes

--- tests/application_tests/test_start_stop.py
+++ tests/application_tests/test_start_stop.py
@@ -33,6 +33,8 @@ class TestStartStop(marqo_test.MarqoTestCase):
             self.client.index(self.index_name_1).search(q="General nature facts")
             raise AssertionError("Marqo is still accessible despite docker stopping!")
         except BackendCommunicationError as mqe:
+            print("Excepted Marqo not accessible error:")
+            print(mqe, "\n")
             pass
 
         start_marqo_res = subprocess.run(["docker", "start", "marqo"], check=True, capture_output=True)
