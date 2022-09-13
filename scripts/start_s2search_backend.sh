2320eb12f15819c5ed0ef3c25016e24bae4e19b2
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-13 10:22:15 +1000

    Update README

--- scripts/start_s2search_backend.sh
+++ scripts/start_s2search_backend.sh
@@ -1,6 +1,6 @@
 docker rm -f marqo;
      docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway \
-        -e "OPENSEARCH_URL=$S2SEARCH_URL" --memory=7g marqo_docker_0 &
+        -e "OPENSEARCH_URL=$S2SEARCH_URL" --memory=7g panduk/mq-experimental:0.0.1 &
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep marqo) ]]; do
     sleep 0.1;
