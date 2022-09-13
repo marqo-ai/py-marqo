aed5e57be07e139fce03d8173e001789667f540c
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-13 23:07:11 +1000

    reactivate clone script, switched to arm64_build branch

--- scripts/start_dind_marqo_os.sh
+++ scripts/start_dind_marqo_os.sh
@@ -1,5 +1,5 @@
 docker rm -f marqo;
-     docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway --memory=7g panduk/mq-experimental:0.0.1 &
+     docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway --memory=7g marqo_docker_0 &
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep marqo) ]]; do
     sleep 0.1;
