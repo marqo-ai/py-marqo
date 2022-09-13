133c0f2b9a176c5268e8901195692a59426b34b8
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-13 22:56:46 +1000

    reactivate clone script, switched to arm64_build branch

--- scripts/clone_marqo_repo.sh
+++ scripts/clone_marqo_repo.sh
@@ -4,17 +4,18 @@
 export MARQO_API_TESTS_ROOT=$(pwd)
 
 . "${MARQO_API_TESTS_ROOT}/conf"
-#export LOCAL_OPENSEARCH_URL="https://localhost:9200"
-#
-#if [[ $(pwd | grep -v marqo-api-tests) ]]; then
-#  exit 1
-#fi
-#rm -rf "${MARQO_API_TESTS_ROOT}/temp"
-#mkdir "${MARQO_API_TESTS_ROOT}/temp"
-#cd "${MARQO_API_TESTS_ROOT}/temp" || exit
-#git clone https://github.com/marqo-ai/marqo.git
-#cd "${MARQO_API_TESTS_ROOT}/temp/marqo" || exit
-#git fetch
-#git switch "$2"
-#
-#DOCKER_BUILDKIT=1 docker build . -t marqo_docker_0 || exit 1
+export LOCAL_OPENSEARCH_URL="https://localhost:9200"
+
+# make sure we're in the correct dir...
+if [[ $(pwd | grep -v marqo-api-tests) ]]; then
+  exit 1
+fi
+rm -rf "${MARQO_API_TESTS_ROOT}/temp"
+mkdir "${MARQO_API_TESTS_ROOT}/temp"
+cd "${MARQO_API_TESTS_ROOT}/temp" || exit
+git clone https://github.com/marqo-ai/marqo.git
+cd "${MARQO_API_TESTS_ROOT}/temp/marqo" || exit
+git fetch
+git switch "$2"
+
+DOCKER_BUILDKIT=1 docker build . -t marqo_docker_0 || exit 1
