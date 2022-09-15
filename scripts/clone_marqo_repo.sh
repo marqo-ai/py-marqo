671751d031433dd5f0aa7e9ee5b43a2c495c64b6
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-15 15:18:03 +1000

    tests for the presence of 'runner/work' in the pwd
    This is because it will be more stable relative to GitHub CI

--- scripts/clone_marqo_repo.sh
+++ scripts/clone_marqo_repo.sh
@@ -7,7 +7,7 @@ export MARQO_API_TESTS_ROOT=$(pwd)
 . "${MARQO_API_TESTS_ROOT}/conf"
 export LOCAL_OPENSEARCH_URL="https://localhost:9200"
 
-if [[ $(pwd | grep -v marqo-api-tests) && $(pwd | grep -v marqo_CI) ]]; then
+if [[ $(pwd | grep -v marqo-api-tests) && $(pwd | grep -v runner/work) ]]; then
   echo checked pwd, and it does not look the directory is correct
   exit 1
 fi
