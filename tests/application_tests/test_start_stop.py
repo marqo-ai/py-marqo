350636f1e3dea77a02b8de324fd41dc4dfd0f1c8
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-22 11:05:01 +1000

    added X number of restarts to number_of_restarts()

--- tests/application_tests/test_start_stop.py
+++ tests/application_tests/test_start_stop.py
@@ -18,39 +18,47 @@ class TestStartStop(marqo_test.MarqoTestCase):
             pass
 
     def test_start_stop(self):
-        # 1 retry a second...
-        NUMBER_OF_TRIES = 100
 
-        d1 = {"Title": "The colour of plants", "_id": "fact_1"}
-        d2 = {"Title": "some frogs", "_id": "fact_2"}
-        self.client.index(self.index_name_1).add_documents(documents=[d1, d2])
-        search_res_0 = self.client.index(self.index_name_1).search(q="General nature facts")
-        assert (search_res_0["hits"][0]["_id"] == "fact_1") or (search_res_0["hits"][0]["_id"] == "fact_2")
-        assert len(search_res_0["hits"]) == 2
+        NUMBER_OF_RESTARTS = 5
 
-        stop_marqo_res = subprocess.run(["docker", "stop", "marqo"], check=True, capture_output=True)
-        assert "marqo" in str(stop_marqo_res.stdout)
+        def run_start_stop():
+            # 1 retry a second...
+            NUMBER_OF_TRIES = 100
 
-        try:
-            self.client.index(self.index_name_1).search(q="General nature facts")
-            raise AssertionError("Marqo is still accessible despite docker stopping!")
-        except BackendCommunicationError as mqe:
-            pass
+            d1 = {"Title": "The colour of plants", "_id": "fact_1"}
+            d2 = {"Title": "some frogs", "_id": "fact_2"}
+            self.client.index(self.index_name_1).add_documents(documents=[d1, d2])
+            search_res_0 = self.client.index(self.index_name_1).search(q="General nature facts")
+            assert (search_res_0["hits"][0]["_id"] == "fact_1") or (search_res_0["hits"][0]["_id"] == "fact_2")
+            assert len(search_res_0["hits"]) == 2
 
-        start_marqo_res = subprocess.run(["docker", "start", "marqo"], check=True, capture_output=True)
-        assert "marqo" in str(start_marqo_res.stdout)
+            stop_marqo_res = subprocess.run(["docker", "stop", "marqo"], check=True, capture_output=True)
+            assert "marqo" in str(stop_marqo_res.stdout)
 
-        for i in range(NUMBER_OF_TRIES):
             try:
                 self.client.index(self.index_name_1).search(q="General nature facts")
-                break
+                raise AssertionError("Marqo is still accessible despite docker stopping!")
             except BackendCommunicationError as mqe:
-                if "exceeds your S2Search free tier limit" in str(mqe):
-                    raise mqe
-                if i + 1 >= NUMBER_OF_TRIES:
-                    raise AssertionError("Timeout waiting for Marqo to restart!")
-                time.sleep(1)
+                pass
 
-        search_res_1 = self.client.index(self.index_name_1).search(q="General nature facts")
-        assert search_res_1["hits"] == search_res_0["hits"]
-        assert (search_res_1["hits"][0]["_id"] == "fact_1") or (search_res_1["hits"][0]["_id"] == "fact_2")
+            start_marqo_res = subprocess.run(["docker", "start", "marqo"], check=True, capture_output=True)
+            assert "marqo" in str(start_marqo_res.stdout)
+
+            for i in range(NUMBER_OF_TRIES):
+                try:
+                    self.client.index(self.index_name_1).search(q="General nature facts")
+                    break
+                except BackendCommunicationError as mqe:
+                    if "exceeds your S2Search free tier limit" in str(mqe):
+                        raise mqe
+                    if i + 1 >= NUMBER_OF_TRIES:
+                        raise AssertionError("Timeout waiting for Marqo to restart!")
+                    time.sleep(1)
+
+            search_res_1 = self.client.index(self.index_name_1).search(q="General nature facts")
+            assert search_res_1["hits"] == search_res_0["hits"]
+            assert (search_res_1["hits"][0]["_id"] == "fact_1") or (search_res_1["hits"][0]["_id"] == "fact_2")
+            return True
+
+        for _ in range(NUMBER_OF_RESTARTS):
+            assert run_start_stop()
