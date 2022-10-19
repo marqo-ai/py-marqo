068ae8a38e4621f9ac9a60699c6edbfded9a0d8f
Pandu Oliver Kerr <pandu@s2search.io>
2022-10-20 08:42:40 +1100

    fixed capitalisation for grep Marqo

--- scripts/start_s2search_backend.sh
+++ scripts/start_s2search_backend.sh
@@ -6,7 +6,7 @@ docker rm -f marqo;
      docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway \
         -e "OPENSEARCH_URL=$S2SEARCH_URL" --memory=7g "$1" &
 # wait for marqo to start
-until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep marqo) ]]; do
+until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
     sleep 0.1;
 done;
 
