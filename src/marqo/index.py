4d88305873c14502229d2f0b33561d77b96ec6e4
aryanagarwal9 <59826369+aryanagarwal9@users.noreply.github.com>
2022-12-22 20:17:50 +0530

    Added get_settings method

--- src/marqo/index.py
+++ src/marqo/index.py
@@ -396,3 +396,6 @@ class Index:
         mq_logger.info('completed batch ingestion.')
         return results
 
+    def get_settings(self) -> List[str, Dict, Dict]:
+        """Get all settings for the index"""
+        return self.http.get(path=f"indexes/{self.index_name}/settings")
