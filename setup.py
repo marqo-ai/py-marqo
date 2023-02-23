2a213b2cf3ca1cba736b4d9705f6e21e1e0d08b0
pandu-k <107458762+pandu-k@users.noreply.github.com>
2023-02-23 23:46:04 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="0.7.0",
+    version="0.7.1",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
