535af848f77ea0f5d06a1e2fda3989ebc576111c
pandu-k <107458762+pandu-k@users.noreply.github.com>
2023-03-17 10:10:59 +1100

    Update version.py

--- src/marqo/version.py
+++ src/marqo/version.py
@@ -1,5 +1,5 @@
-__marqo_version__ = "0.0.15"
-__marqo_release_page__ = "https://github.com/marqo-ai/marqo/releases/tag/0.0.15"
+__marqo_version__ = "0.0.16"
+__marqo_release_page__ = "https://github.com/marqo-ai/marqo/releases/tag/0.0.16"
 
 
 def supported_marqo_version() -> str:
