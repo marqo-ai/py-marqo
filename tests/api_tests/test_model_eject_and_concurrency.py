7cf6f89978c5efede5691121f0446fb9ab7734b8
Li Wan <lwan3@student.unimelb.edu.au>
2023-03-22 17:09:12 +1100

    catch mainline

--- tests/api_tests/test_model_eject_and_concurrency.py
+++ tests/api_tests/test_model_eject_and_concurrency.py
@@ -21,16 +21,16 @@ class TestModelEjectAndConcurrency(MarqoTestCase):
             "test_1": 'open_clip/ViT-B-32/laion400m_e32',
             "test_2": 'open_clip/RN50x4/openai',
             "test_3": 'onnx16/open_clip/RN50-quickgelu/openai',
-            "test_4": "onnx16/open_clip/ViT-L-14/laion2b_s32b_b82k",
-            "test_5": "onnx32/open_clip/ViT-L-14/openai",
-            "test_6": "hf/all-MiniLM-L6-v1",
-            "test_7": "hf/all-MiniLM-L6-v2",
-            "test_8": "hf/all_datasets_v3_MiniLM-L12",
-            "test_9": 'open_clip/ViT-B-32/laion2b_e16',
-            "test_10": 'ViT-B/16',
-            "test_11": 'ViT-L/14@336px',
-            "test_12": "onnx16/openai/ViT-L/14",
-            "test_13": 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
+            # "test_4": "onnx16/open_clip/ViT-L-14/laion2b_s32b_b82k",
+            # "test_5": "onnx32/open_clip/ViT-L-14/openai",
+            # "test_6": "hf/all-MiniLM-L6-v1",
+            # "test_7": "hf/all-MiniLM-L6-v2",
+            # "test_8": "hf/all_datasets_v3_MiniLM-L12",
+            # "test_9": 'open_clip/ViT-B-32/laion2b_e16',
+            # "test_10": 'ViT-B/16',
+            # "test_11": 'ViT-L/14@336px',
+            # "test_12": "onnx16/openai/ViT-L/14",
+            # "test_13": 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
         }
 
         for index_name, model in cls.index_model_object.items():
@@ -77,30 +77,30 @@ class TestModelEjectAndConcurrency(MarqoTestCase):
                 q.put(e)
             pass
 
-    def test_sequentially_search(self):
-        for index_name in list(self.index_model_object):
-            self.client.index(index_name).search(q='What is the best outfit to wear on the moon?')
+    # def test_sequentially_search(self):
+    #     for index_name in list(self.index_model_object):
+    #         self.client.index(index_name).search(q='What is the best outfit to wear on the moon?')
 
-    def test_concurrent_search_with_cache(self):
-        # Search once to make sure the model is in cache
-        test_index = "test_1"
-        res = self.client.index(test_index).search("what is best to wear on the moon?")
-
-        q = multiprocessing.Queue()
-        processes = []
-        for i in range(5):
-            p = multiprocessing.Process(target=self.normal_search, args=(test_index, q))
-            processes.append(p)
-            p.start()
-
-        for p in processes:
-            p.join()
-
-        assert q.empty()
+    # def test_concurrent_search_with_cache(self):
+    #     # Search once to make sure the model is in cache
+    #     test_index = "test_1"
+    #     res = self.client.index(test_index).search("what is best to wear on the moon?")
+    #
+    #     q = multiprocessing.Queue()
+    #     processes = []
+    #     for i in range(5):
+    #         p = multiprocessing.Process(target=self.normal_search, args=(test_index, q))
+    #         processes.append(p)
+    #         p.start()
+    #
+    #     for p in processes:
+    #         p.join()
+    #
+    #     assert q.empty()
 
     def test_concurrent_search_without_cache(self):
         # Remove all the cached models
-        MarqoTestCase.removeAllModels()
+        super().removeAllModels()
 
         test_index = "test_3"
         q = multiprocessing.Queue()
