e0dc9716ff3c522308b1aa8c7a804601bd486b82
Li Wan <lwan3@student.unimelb.edu.au>
2023-04-03 14:10:57 +1000

    add test

--- tests/v0_tests/test_score_modifier_search.py
+++ tests/v0_tests/test_score_modifier_search.py
@@ -104,7 +104,7 @@ class TestCustomVectorSearch(MarqoTestCase):
         resp = self.client.bulk_search([{
             "index": self.index_name_1,
             "q": "title about some doc",
-            "void" : {
+            "void-void" : {
                 # typo in multiply score by
                 "multiply_score_bys":
                     [{"field_name": "multiply_1",
