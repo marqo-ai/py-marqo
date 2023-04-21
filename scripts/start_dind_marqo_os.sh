75da26223bc8bf786e7dd66984dc51869a40741c
Li Wan <lwan3@student.unimelb.edu.au>
2023-04-21 17:45:03 +1000

    set threshold hold to 1.5

--- scripts/start_dind_marqo_os.sh
+++ scripts/start_dind_marqo_os.sh
@@ -2,7 +2,7 @@
 # args:
 # $1 : marqo_image_name - name of the image you want to test
 docker rm -f marqo;
-     docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway -e "MARQO_MAX_CPU_MODEL_MEMORY=2" --memory=7g "$1" &
+     docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway -e "MARQO_MAX_CPU_MODEL_MEMORY=1.5" --memory=7g "$1" &
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
     sleep 0.1;
