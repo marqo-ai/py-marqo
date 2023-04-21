799d0fe4908d92ec9fcbc8a6998458cc3c746328
Li Wan <lwan3@student.unimelb.edu.au>
2023-04-21 19:04:18 +1000

    set threshold hold to 1.6

--- scripts/start_local_marqo_os.sh
+++ scripts/start_local_marqo_os.sh
@@ -14,7 +14,7 @@ done;
 
 docker rm -f marqo
     docker run --name marqo --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway \
-        -e "MARQO_MAX_CPU_MODEL_MEMORY=1.5" \
+        -e "MARQO_MAX_CPU_MODEL_MEMORY=1.6" \
         -e "OPENSEARCH_URL=$LOCAL_OPENSEARCH_URL" --memory=6g "$1" &
 # wait for marqo to start
 until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
