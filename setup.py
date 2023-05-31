eae24843364ca6cda698db6a7fda1839e9726062
Li Wan <49334982+wanliAlex@users.noreply.github.com>
2023-05-31 18:59:34 +1000

    bump py-marqo version (#98)
    * bump py-marqo version
    
    * bump py-marqo version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="0.9.5",
+    version="0.9.6",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
