705e3694f7d2e6503614ba8f0e1480c5f237c93c
Li Wan <49334982+wanliAlex@users.noreply.github.com>
2023-05-31 15:14:25 +1000

    Extra tests bsearch (#95)
    * add extra tests for bulk search
    
    * add extra tests for bulk search
    
    * add context dimension error test in bulk test
    
    * add tests for context in bulk search
    
    * catch jack's branche
    
    * add scoremodifier in bulk search end to end
    
    * add scoremodifier in bulk search end to end

--- tests/v0_tests/test_custom_vector_search.py
+++ tests/v0_tests/test_custom_vector_search.py
@@ -143,3 +143,39 @@ class TestCustomBulkVectorSearch(TestCustomVectorSearch):
             return resp['result'][0]
         return {}
 
+    def test_context_dimension_error_in_bulk_search(self):
+        correct_context = {"tensor": [{"vector": [1, ] * 384, "weight": 1}, {"vector": [2, ] * 384, "weight": 0}]}
+        wrong_context = {"tensor": [{"vector": [1, ] * 2, "weight": 1}, {"vector": [2, ] * 3, "weight": 0}]}
+        if self.IS_MULTI_INSTANCE:
+            self.warm_request(self.client.bulk_search, [{
+                "index": self.index_name_1,
+                "q": "blah blah",
+                "context": correct_context,
+            }])
+        try:
+            resp = self.client.bulk_search([{
+                "index": self.index_name_1,
+                "q": {"blah blah": 1},
+                "context": wrong_context, # the dimension mismatches the index
+            }])
+            raise AssertionError
+        except MarqoWebError as e:
+            assert "The provided vectors are not in the same dimension of the index" in str(e)
+
+    def test_context_with_query_string_in_bulk_search(self):
+        correct_context = {"tensor": [{"vector": [1, ] * 384, "weight": 1}, {"vector": [2, ] * 384, "weight": 0}]}
+        if self.IS_MULTI_INSTANCE:
+            self.warm_request(self.client.bulk_search, [{
+                "index": self.index_name_1,
+                "q": "blah blah",
+                "context": correct_context,
+            }])
+        try:
+            resp = self.client.bulk_search([{
+                "index": self.index_name_1,
+                "q": "blah blah",
+                "context": correct_context, # the dimension mismatches the index
+            }])
+            raise AssertionError
+        except MarqoWebError as e:
+            assert "This is not supported as the context only works when the query is a dictionary." in str(e)
