b2771bd0d3ddea65a2fef0f6ac5ec21b1d08f109
Li Wan <49334982+wanliAlex@users.noreply.github.com>
2023-07-06 15:14:43 +1000

    fix async tests (#37)
    * remove server_batch_size
    
    * remove server_batch_size
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * change sleep
    
    * fix async test
    
    * fix async test
    
    * fix async test
    
    * fix async test
    
    * fix async test
    
    * fix async test
    
    * fix async test

--- tests/application_tests/test_asynchronous.py
+++ tests/application_tests/test_asynchronous.py
@@ -42,8 +42,7 @@ class TestAsync (marqo_test.MarqoTestCase):
             docs = [{"Title": " ".join(random.choices(population=vocab, k=10)),
                           "Description": " ".join(random.choices(population=vocab, k=25)),
                           } for _ in range(num_docs)]
-            self.client.index(self.index_name_1).add_documents(
-                auto_refresh=True, documents=docs)
+            self.client.index(self.index_name_1).add_documents(documents=docs, auto_refresh=True, client_batch_size=1)
 
         cache_update_thread = threading.Thread(
             target=significant_ingestion)
