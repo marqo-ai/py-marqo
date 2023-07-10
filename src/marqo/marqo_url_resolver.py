b89b6f0108decc7583459caa943add994d3da13c
danyilq <danyil@s2search.io>
2023-07-10 21:12:30 +0200

    improves for edgecases

--- src/marqo/marqo_url_resolver.py
+++ src/marqo/marqo_url_resolver.py
@@ -4,7 +4,7 @@ import requests
 
 
 class MarqoUrlResolver:
-    def __init__(self, api_key=None, expiration_time: int = 60):
+    def __init__(self, api_key=None, expiration_time: int = 15):
         """ URL Resolver is a cache for urls that are resolved to their respective indices only for marqo cloud. """
         self.timestamp = time.time() - expiration_time
         self._urls_mapping = {}
@@ -13,9 +13,11 @@ class MarqoUrlResolver:
 
     def refresh_urls_if_needed(self, index_name):
         if index_name not in self._urls_mapping and time.time() - self.timestamp > self.expiration_time:
+            # fast refresh to catch if index was created
             self._refresh_urls()
-            if self._urls_mapping:
-                self.timestamp = time.time()
+        if index_name in self._urls_mapping and time.time() - self.timestamp > 360:
+            # slow refresh in case index was deleted
+            self._refresh_urls()
 
     @property
     def urls_mapping(self):
@@ -28,3 +30,5 @@ class MarqoUrlResolver:
             index['index_name']: index['load_balancer_dns_name'] for index in response['indices']
             if index['index_status'] == "READY"
         }
+        if self._urls_mapping:
+            self.timestamp = time.time()
