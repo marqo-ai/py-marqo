f1d772cecb254c1b2affbbd19fc448e62b5dab8a
Joshua Kim <joshua@marqo.ai>
2023-07-17 16:36:36 +1000

    fixed search and bulk search filtering tests

--- tests/v0_tests/test_bulk_search.py
+++ tests/v0_tests/test_bulk_search.py
@@ -281,42 +281,96 @@ class TestBulkSearch(MarqoTestCase):
         args, _ = mock__post.call_args_list[0]
         assert "device" not in args[0]
 
-    def test_prefiltering(self):
+    def test_filter_string_and_searchable_attributes(self):
         self.client.create_index(index_name=self.index_name_1)
-        d1 = {
-            "doc title": "Very heavy, dense metallic lead.",
-            "abc-123": "some text",
-            "an_int": 2,
-            "_id": "my-cool-doc"
-        }
-        d2 = {
-            "doc title": "The captain bravely lead her followers into battle."
-                         " She directed her soldiers to and fro.",
-            "field X": "this is a solid doc",
-            "field1": "other things",
-            "_id": "123456"
-        }
-        res = self.client.index(self.index_name_1).add_documents([
-            d1, d2
-        ],auto_refresh=True)
+        docs = [
+            {
+                "_id": "0",                     # content in field_a
+                "field_a": "random content",
+                "str_for_filtering": "apple",
+                "int_for_filtering": 0,
+            },
+            {
+                "_id": "1",                     # content in field_b
+                "field_b": "random content",
+                "str_for_filtering": "banana",
+                "int_for_filtering": 0,
+            },
+            {
+                "_id": "2",                     # content in both
+                "field_a": "random content",
+                "field_b": "random content",
+                "str_for_filtering": "apple",
+                "int_for_filtering": 1,
+            },
+            {
+                "_id": "3",                     # content in both
+                "field_a": "random content",
+                "field_b": "random content",
+                "str_for_filtering": "banana",
+                "int_for_filtering": 1,
+            }
+        ]
+        res = self.client.index(self.index_name_1).add_documents(docs,auto_refresh=True)
 
-        if self.IS_MULTI_INSTANCE:
-            self.warm_request(self.client.bulk_search, [{
+        test_cases = (
+            {   # filter string only (str)
+                "query": "random content", 
+                "filter_string": "str_for_filtering:apple", 
+                "searchable_attributes": None,
+                "expected": ["0", "2"]
+            },  
+            {   # filter string only (int)
+                "query": "random content", 
+                "filter_string": "int_for_filtering:0", 
+                "searchable_attributes": None,
+                "expected": ["0", "1"]
+            },  
+            {   # filter string only (str and int)
+                "query": "random content", 
+                "filter_string": "str_for_filtering:banana AND int_for_filtering:1", 
+                "searchable_attributes": None,
+                "expected": ["3"]
+            },  
+            {   # searchable attributes only (one)
+                "query": "random content", 
+                "filter_string": None,
+                "searchable_attributes": ["field_b"], 
+                "expected": ["1", "2", "3"]
+            },   
+            {   # searchable attributes only (both)
+                "query": "random content", 
+                "filter_string": None,
+                "searchable_attributes": ["field_a", "field_b"], 
+                "expected": ["0", "1", "2", "3"]
+            },         
+            {   # filter string and searchable attributes (one)
+                "query": "random content",
+                "filter_string": "str_for_filtering:apple",
+                "searchable_attributes": ["field_b"],
+                "expected": ["2"]
+            },
+            {   # filter string and searchable attributes (both)
+                "query": "random content",
+                "filter_string": "str_for_filtering:banana AND int_for_filtering:0",
+                "searchable_attributes": ["field_a"],
+                "expected": []
+            }
+        )
+
+        for case in test_cases:
+            query_object_0 = {
                 "index": self.index_name_1,
-                "q": "blah blah",
-                "filter": "(an_int:[0 TO 30] and an_int:2) AND abc-123:(some text)"
-            }])
-
-        resp = self.client.bulk_search([{
-            "index": self.index_name_1,
-            "q": "blah blah",
-            "filter": "(an_int:[0 TO 30] and an_int:2) AND abc-123:(some text)"
-        }])
-        assert len(resp['result']) == 1
-        search_res = resp['result'][0]
-
-        assert len(search_res["hits"]) == 1
-        assert search_res["hits"][0]["_id"] == "my-cool-doc"
+                "q": case["query"],
+                "filter": case.get("filter_string", ""),
+                "searchableAttributes": case.get("searchable_attributes", None)
+            }
+            if self.IS_MULTI_INSTANCE:
+                self.warm_request(self.client.bulk_search, [query_object_0])
+            search_res = self.client.bulk_search([query_object_0])["result"][0]
+            assert len(search_res["hits"]) == len(case["expected"])
+            assert set([hit["_id"] for hit in search_res["hits"]]) == set(case["expected"])
+            
 
     def test_attributes_to_retrieve(self):
         self.client.create_index(index_name=self.index_name_1)
