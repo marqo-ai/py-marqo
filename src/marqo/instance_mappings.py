8ee578f00f0ca925624cccb5fd8a23c4406cf603
Farshid Zavareh <farshid@marqo.ai>
2023-08-02 15:54:45 +1000

    Add docstring for InstanceMappings

--- src/marqo/instance_mappings.py
+++ src/marqo/instance_mappings.py
@@ -4,6 +4,12 @@ from typing import Dict, Optional
 
 
 class InstanceMappings(ABC):
+    """
+    Abstract class for mapping index names to URLs.
+
+    Implementations of this class must ensure at least asymptotically constant computational complexity for all methods.
+    An inefficient implementation of this class can cause the Marqo client to be slow.
+    """
     @abstractmethod
     def get_url(self, index_name: str) -> str:
         """
