58a89ff0fc904e6dbbbbe39084539f4a0aa1262e
Farshid Zavareh <farshid@marqo.ai>
2023-08-14 22:18:11 +1000

    Change version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="1.2.4",
+    version="1.2.3",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
