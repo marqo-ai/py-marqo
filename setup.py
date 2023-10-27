bc14e155ea0d1bdea31701e6eaf74ec806ba9a29
Joshua <joshua@marqo.ai>
2023-10-27 14:30:11 +1100

    Update client version to 2.0.0 (#183)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="1.3.1",
+    version="2.0.0",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
