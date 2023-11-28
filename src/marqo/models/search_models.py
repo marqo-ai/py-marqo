5f6db4f76ac0be32723064a929e1cd562f2ede72
Joshua Kim <joshua@marqo.ai>
2023-11-29 00:19:06 +0800

    fix bulk search prefix

--- src/marqo/models/search_models.py
+++ src/marqo/models/search_models.py
@@ -23,7 +23,7 @@ class SearchBody(BaseMarqoModel):
     context: Optional[Dict] = None
     scoreModifiers: Optional[Dict] = None
     modelAuth: Optional[Dict] = None
-    testQueryPrefix: Optional[str] = None
+    textQueryPrefix: Optional[str] = None
 
 
 class BulkSearchBody(SearchBody):
