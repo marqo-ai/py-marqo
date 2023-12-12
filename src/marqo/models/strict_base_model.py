c95e7556c25006fb3fbc2fff0c16ba8c8037eb7b
Li Wan <lwan3@student.unimelb.edu.au>
2023-12-12 12:00:10 +1100

    Remove allow_population_by_field_name for StrictBaseModel

--- src/marqo/models/strict_base_model.py
+++ src/marqo/models/strict_base_model.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 class StrictBaseModel(BaseModel):
     class Config:
         extra: str = "forbid"
-        allow_population_by_field_name = True
 
 
 class StrictAllowPopulationBaseModel(BaseModel):
