adcd62995318f23170ac374f29b82c01210a2396
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-02 11:18:12 +1100

    Change test index name to test_index for cloud

--- tests/cloud_test_logic/cloud_test_index.py
+++ tests/cloud_test_logic/cloud_test_index.py
@@ -26,41 +26,41 @@ index_name_to_settings_mappings = {
         "inferenceType": "marqo.CPU.large",
         "storageClass": "marqo.basic"
     },
-    CloudTestIndex.unstructured_image_index: {
-        "type": "unstructured",
-        "treatUrlsAndPointersAsImages": True,
-        "model": "open_clip/ViT-B-32/laion400m_e32",
-        "inferenceType": "marqo.CPU.large",
-        "storageClass": "marqo.basic"
-    },
-    CloudTestIndex.unstructured_text_index_with_custom_model: {
-        "type": "unstructured",
-        "treatUrlsAndPointersAsImages": False,
-        "model": "test-model",
-        "modelProperties": {
-            "name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
-            "dimensions": 384,
-            "tokens": 128,
-            "type": "sbert"
-        },
-        "normalizeEmbeddings": True,
-        "textPreprocessing": {
-            "splitLength": 2,
-            "splitOverlap": 1,
-        },
-        "inferenceType": "marqo.CPU.large",
-        "storageClass": "marqo.basic"
-    },
-    CloudTestIndex.unstructured_image_index_with_preprocessing_method: {
-        "type": "unstructured",
-        "treatUrlsAndPointersAsImages": True,
-        "model": "open_clip/ViT-B-16/laion400m_e31",
-        "imagePreprocessing": {
-            "patchMethod": "simple"
-        },
-        "inferenceType": "marqo.CPU.large",
-        "storageClass": "marqo.basic"
-    },
+    # CloudTestIndex.unstructured_image_index: {
+    #     "type": "unstructured",
+    #     "treatUrlsAndPointersAsImages": True,
+    #     "model": "open_clip/ViT-B-32/laion400m_e32",
+    #     "inferenceType": "marqo.CPU.large",
+    #     "storageClass": "marqo.basic"
+    # },
+    # CloudTestIndex.unstructured_text_index_with_custom_model: {
+    #     "type": "unstructured",
+    #     "treatUrlsAndPointersAsImages": False,
+    #     "model": "test-model",
+    #     "modelProperties": {
+    #         "name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
+    #         "dimensions": 384,
+    #         "tokens": 128,
+    #         "type": "sbert"
+    #     },
+    #     "normalizeEmbeddings": True,
+    #     "textPreprocessing": {
+    #         "splitLength": 2,
+    #         "splitOverlap": 1,
+    #     },
+    #     "inferenceType": "marqo.CPU.large",
+    #     "storageClass": "marqo.basic"
+    # },
+    # CloudTestIndex.unstructured_image_index_with_preprocessing_method: {
+    #     "type": "unstructured",
+    #     "treatUrlsAndPointersAsImages": True,
+    #     "model": "open_clip/ViT-B-16/laion400m_e31",
+    #     "imagePreprocessing": {
+    #         "patchMethod": "simple"
+    #     },
+    #     "inferenceType": "marqo.CPU.large",
+    #     "storageClass": "marqo.basic"
+    # },
     # Structured indexes
     CloudTestIndex.structured_basic_index: {
         "type": "structured",
@@ -69,45 +69,45 @@ index_name_to_settings_mappings = {
         "tensorFields": ["text_field_1"],
         "storageClass": "marqo.basic"
     },
-    CloudTestIndex.structured_image_index: {
-        "type": "structured",
-        "treatUrlsAndPointersAsImages": True,
-        "model": "open_clip/ViT-B-32/laion400m_e32",
-        "allFields": [{"name": "text_field_1", "type": "text"}],
-        "tensorFields": ["text_field_1"],
-        "inferenceType": "marqo.CPU.large",
-        "storageClass": "marqo.basic"
-    },
-    CloudTestIndex.structured_text_index_with_custom_model: {
-        "type": "structured",
-        "treatUrlsAndPointersAsImages": False,
-        "model": "test-model",
-        "allFields": [{"name": "text_field_1", "type": "text"}],
-        "tensorFields": ["text_field_1"],
-        "modelProperties": {
-            "name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
-            "dimensions": 384,
-            "tokens": 128,
-            "type": "sbert"
-        },
-        "normalizeEmbeddings": True,
-        "textPreprocessing": {
-            "splitLength": 2,
-            "splitOverlap": 1,
-        },
-        "inferenceType": "marqo.CPU.large",
-        "storageClass": "marqo.basic"
-    },
-    CloudTestIndex.structured_image_index_with_preprocessing_method: {
-        "type": "structured",
-        "treatUrlsAndPointersAsImages": True,
-        "model": "open_clip/ViT-B-16/laion400m_e31",
-        "allFields": [{"name": "text_field_1", "type": "text"}],
-        "tensorFields": ["text_field_1"],
-        "imagePreprocessing": {
-            "patchMethod": "simple"
-        },
-        "inferenceType": "marqo.CPU.large",
-        "storageClass": "marqo.basic"
-    },
+    # CloudTestIndex.structured_image_index: {
+    #     "type": "structured",
+    #     "treatUrlsAndPointersAsImages": True,
+    #     "model": "open_clip/ViT-B-32/laion400m_e32",
+    #     "allFields": [{"name": "text_field_1", "type": "text"}],
+    #     "tensorFields": ["text_field_1"],
+    #     "inferenceType": "marqo.CPU.large",
+    #     "storageClass": "marqo.basic"
+    # },
+    # CloudTestIndex.structured_text_index_with_custom_model: {
+    #     "type": "structured",
+    #     "treatUrlsAndPointersAsImages": False,
+    #     "model": "test-model",
+    #     "allFields": [{"name": "text_field_1", "type": "text"}],
+    #     "tensorFields": ["text_field_1"],
+    #     "modelProperties": {
+    #         "name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
+    #         "dimensions": 384,
+    #         "tokens": 128,
+    #         "type": "sbert"
+    #     },
+    #     "normalizeEmbeddings": True,
+    #     "textPreprocessing": {
+    #         "splitLength": 2,
+    #         "splitOverlap": 1,
+    #     },
+    #     "inferenceType": "marqo.CPU.large",
+    #     "storageClass": "marqo.basic"
+    # },
+    # CloudTestIndex.structured_image_index_with_preprocessing_method: {
+    #     "type": "structured",
+    #     "treatUrlsAndPointersAsImages": True,
+    #     "model": "open_clip/ViT-B-16/laion400m_e31",
+    #     "allFields": [{"name": "text_field_1", "type": "text"}],
+    #     "tensorFields": ["text_field_1"],
+    #     "imagePreprocessing": {
+    #         "patchMethod": "simple"
+    #     },
+    #     "inferenceType": "marqo.CPU.large",
+    #     "storageClass": "marqo.basic"
+    # },
 }
