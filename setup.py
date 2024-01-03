5709ca30721dfc18769acbf70142fc2380e4aa20
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-03 20:47:12 +1100

    pin requirements version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         # client:
         "requests",
-        "urllib3<2.0.0",
+        "urllib3<2.0.0, >=1.26.0",
         "pydantic<2.0.0",
         "typing-extensions>=4.5.0",
         "packaging"
