ac8817868e7bfab4c1c1ca842acae4c4b0b70327
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-16 11:30:45 +1100

    Read package name from env variable

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+import os
 # from src.marqo.version import __version__
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
+
+
+def get_package_name() -> str:
+    return os.getenv("MARQO_PACKAGE_NAME", "marqo1")
+
 
 setup(
     install_requires=[
@@ -18,6 +24,7 @@ setup(
         "tox"
     ],
     author="marqo org",
+    name=get_package_name(),
     author_email="org@marqo.io",
     description="Tensor search for humans",
     long_description=long_description,
