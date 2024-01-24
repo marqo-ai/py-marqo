eed257777c19862dcd31771140951c95aef3ad79
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-24 22:17:42 +1100

    Update tests

--- tests/v2_tests/test_add_documents.py
+++ tests/v2_tests/test_add_documents.py
@@ -733,8 +733,8 @@ class TestAddDocuments(MarqoTestCase):
         test_documents = [
             ({"long_field_1": 1}, False),  # small positive integer
             ({"long_field_1": -1}, False),  # small negative integer
-            ({"long_field_1": 100232142}, False),  # large positive integer
-            ({"long_field_1": -923217213}, False),  # large positive integer
+            ({"long_field_1": 1002321422345}, False),  # large positive integer that can't be handled by int
+            ({"long_field_1": -9232172132133}, False),  # large positive integer that can't be handled by int
             ({'long_field_1': int("1" * 50)}, True),  # overlarge positive integer, should raise error in long field
             # overlarge negative integer, should raise error in long field
             ({'long_field_1': -1 * int("1" * 50)}, True),
