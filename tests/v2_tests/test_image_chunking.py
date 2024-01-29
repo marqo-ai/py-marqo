921c185762312c435056de8263d1af8e774d9fdf
danyilq <107251367+danyilq@users.noreply.github.com>
2024-01-29 02:02:46 +0200

    Danyil/fix cloud cleanup (#216)
    * add image chunking test
    
    * fix index cleanup
    
    * fix indexes cleanup
    
    * change too long idx name
    
    * Add check for too long index name

--- tests/v2_tests/test_image_chunking.py
+++ tests/v2_tests/test_image_chunking.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from PIL import Image
 from marqo.client import Client
@@ -11,7 +13,6 @@ class TestImageChunking(MarqoTestCase):
     """Test for image chunking as a preprocessing step
     """
 
-    @mark.fixed
     def test_image_no_chunking(self):
 
         image_size = (256, 384)
@@ -58,6 +59,7 @@ class TestImageChunking(MarqoTestCase):
             # the highlight should be the location
             assert results['hits'][0]['_highlights'][0]['image_field_1'] == temp_file_name
 
+    @mark.fixed
     def test_image_simple_chunking(self):
 
         image_size = (256, 384)
@@ -70,51 +72,63 @@ class TestImageChunking(MarqoTestCase):
                 pass
 
         settings = {
-            "treat_urls_and_pointers_as_images":True,   # allows us to find an image file and index it 
-            "model":"ViT-B/16",
-            "image_preprocessing_method":"simple"
+            "type": "structured",
+        "model": "open_clip/ViT-B-32/laion2b_s34b_b79k",
+        "allFields": [
+            {"name": "text_field_1", "type": "text", "features": ["lexical_search", "filter"]},
+            {"name": "text_field_2", "type": "text", "features": ["lexical_search", "filter"]},
+            {"name": "image_field_1", "type": "image_pointer"},
+            ],
+            "tensorFields": ["text_field_1", "text_field_2", "image_field_1"],
+            "imagePreprocessing": {
+                "patchMethod": "simple",
+            },
             }
 
-        test_index_name = self.create_test_index(
-            cloud_test_index_to_use=CloudTestIndex.image_index_with_preprocessing_method,
-            open_source_test_index_name=self.generic_test_index_name,
-            open_source_index_kwargs=settings,
+        test_index_name = self.get_test_index_name(
+            cloud_test_index_to_use=CloudTestIndex.structured_image,
+            open_source_test_index_name=None
         )
+        if not self.client.config.is_marqo_cloud:
+            self.client.create_index(self.generic_test_index_name, settings_dict=settings)
+            test_index_name = self.generic_test_index_name
         temp_file_name = 'https://avatars.githubusercontent.com/u/13092433?v=4'
         
         img = Image.open(requests.get(temp_file_name, stream=True).raw)
 
-        document1 = {'_id': '1', # '_id' can be provided but is not required
-            'attributes': 'hello',
-            'description': 'the image chunking can (optionally) chunk the image into sub-patches (akin to segmenting text) by using either a learned model or simple box generation and cropping',
-            'location': temp_file_name}
+        document1 = {'_id': '1',  # '_id' can be provided but is not required
+            'text_field_1': 'hello',
+            'text_field_2': 'the image chunking can (optionally) chunk the image into sub-patches (akin to segmenting text) by using either a learned model or simple box generation and cropping',
+            'image_field_1': temp_file_name}
 
-        client.index(test_index_name).add_documents([document1], tensor_fields=['location', 'description', 'attributes'])
+        client.index(test_index_name).add_documents([document1])
 
         # test the search works
         if self.IS_MULTI_INSTANCE:
             self.warm_request(client.index(test_index_name).search,'a')
 
         results = client.index(test_index_name).search('a')
-        print(results)
-        assert results['hits'][0]['location'] == temp_file_name
+        assert results['hits'][0]['image_field_1'] == temp_file_name
 
         # search only the image location
         if self.IS_MULTI_INSTANCE:
-            self.warm_request(client.index(test_index_name).search,'a', searchable_attributes=['location'])
+            self.warm_request(client.index(test_index_name).search,'a', searchable_attributes=['image_field_1'])
 
-        results = client.index(test_index_name).search('a', searchable_attributes=['location'])
-        print(results)
-        assert results['hits'][0]['location'] == temp_file_name
+        results = client.index(test_index_name).search('a', searchable_attributes=['image_field_1'])
+        assert results['hits'][0]['image_field_1'] == temp_file_name
         # the highlight should be the location
-        assert results['hits'][0]['_highlights']['location'] != temp_file_name
-        assert len(results['hits'][0]['_highlights']['location']) == 4
-        assert all(isinstance(_n, (float, int)) for _n in results['hits'][0]['_highlights']['location'])
+        assert json.loads(results['hits'][0]['_highlights'][0]['image_field_1']) != temp_file_name
+        assert len(json.loads(results['hits'][0]['_highlights'][0]['image_field_1'])) == 4
+        assert all(
+            isinstance(_n, (float, int)) for _n in json.loads(results['hits'][0]['_highlights'][0]['image_field_1'])
+        )
 
         # search using the image itself, should return a full sized image as highlight
         if self.IS_MULTI_INSTANCE:
             self.warm_request(client.index(test_index_name).search,temp_file_name)
 
         results = client.index(test_index_name).search(temp_file_name)
-        print(results)
-        assert abs(np.array(results['hits'][0]['_highlights']['location']) - np.array([0, 0, img.size[0], img.size[1]])).sum() < 1e-6
+        assert abs(np.array(json.loads(results['hits'][0]['_highlights'][0]['image_field_1'])) - np.array([0, 0, img.size[0], img.size[1]])).sum() < 1e-6
+
+        if not self.client.config.is_marqo_cloud:
+            self.client.delete_index(test_index_name)
