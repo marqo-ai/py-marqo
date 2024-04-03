3ace12a933da6b4fa40d184372766c03cc656e72
Joshua <joshua@marqo.ai>
2024-04-04 02:46:26 +0800

    bump marqo version

--- src/marqo/version.py
+++ src/marqo/version.py
@@ -1,4 +1,4 @@
-__marqo_version__ = "2.3.0"
+__marqo_version__ = "2.4.0"
 __marqo_release_page__ = f"https://github.com/marqo-ai/marqo/releases/tag/{__marqo_version__}"
 
 __minimum_supported_marqo_version__ = "2.0"
