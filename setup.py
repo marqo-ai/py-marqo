8f14c11dbccafd51f7854df364ddead3d8df819f
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-05-14 15:01:33 +0800

    change pydantic version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         # client:
         "requests",
         "urllib3<2.0.0, >=1.26.0",
-        "pydantic<2.0.0",
+        "pydantic>=2.0.0",
         "typing-extensions>=4.5.0",
         "packaging"
     ],
