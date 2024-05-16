fc6e9bc71e34ead566ced611fa3ead9240d62638
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-05-16 14:19:51 +0800

    change minor version to 3.5.0 from 3.4.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="3.4.1",
+    version="3.5.0",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
