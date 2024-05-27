a6398ab8660eed3c5f29a833c321744f9a0d2fab
Li Wan <49334982+wanliAlex@users.noreply.github.com>
2024-05-27 11:12:23 +1000

    Update py-marqo version to 3.5.1 and Marqo verison to 2.7.0 (#236)
    We publish a new py-marqo version 3.5.1. The `__minimum_required_marqo_version__` is updated to 2.6.0.

--- src/marqo/version.py
+++ src/marqo/version.py
@@ -1,7 +1,7 @@
-__marqo_version__ = "2.6.0"
+__marqo_version__ = "2.7.0"
 __marqo_release_page__ = f"https://github.com/marqo-ai/marqo/releases/tag/{__marqo_version__}"
 
-__minimum_supported_marqo_version__ = "2.0"
+__minimum_supported_marqo_version__ = "2.6.0"
 
 
 def supported_marqo_version() -> str:
