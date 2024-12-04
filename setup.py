6c82d4bf22a98d6e60bc15a86d6e64c2348a5fb6
yihanzhao <yihan@marqo.ai>
2024-12-04 11:06:08 +1100

    bump up the version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="3.9.1",
+    version="3.9.2",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
