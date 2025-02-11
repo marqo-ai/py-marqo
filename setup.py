a256918bd425f8b91cd33125afcaec9557f07fd6
Joshua <joshua@marqo.ai>
2025-02-11 11:29:35 +0800

    version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="3.10.0",
+    version="3.11.0",
     author="marqo org",
     author_email="org@marqo.io",
     description="Tensor search for humans",
