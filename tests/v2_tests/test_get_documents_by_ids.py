b97cf7c120372ba2ea14caf0e5b53485977a7ed2
Li Wan <li@marqo.ai>
2025-04-29 13:22:48 +1000

    Add tests for get_documents_by_ids_via_get

---
+++ tests/v2_tests/test_get_documents_by_ids.py
@@ -0,0 +1,74 @@
+import time
+from unittest import mock
+
+import numpy as np
+import pytest
+import requests
+from pytest import mark
+
+from marqo.errors import MarqoError, MarqoWebError
+from tests.marqo_test import MarqoTestCase, CloudTestIndex
+import requests
+
+
+@mark.fixed
+class TestGetDocumentsByIds(MarqoTestCase):
+    def get_documents_by_ids_via_get(self, index_name, document_ids, expose_facets=False):
+        body = {
+            "documentIds": document_ids,
+        }
+
+        url = self.authorized_url
+        api_key = self.client_settings.get("api_key", None)
+
+        if api_key:
+            headers = {"x-api-key": self.client_settings["api_key"]}
+        else:
+            headers = {}
+
+        url = f"{url}/indexes/{index_name}/documents/get-batch"
+
+        if expose_facets:
+            url += f"?expose_facets={expose_facets}"
+
+        return requests.post(url, json=body, headers=headers)
+
+    def test_add_documents_by_ids(self):
+        """Ensure get_documents_by_ids works for both GET and POST requests."""
+        for cloud_test_index_to_use, open_source_test_index_name in self.test_cases:
+            test_index_name = self.get_test_index_name(
+                cloud_test_index_to_use=cloud_test_index_to_use,
+                open_source_test_index_name=open_source_test_index_name
+            )
+            with (self.subTest(test_index_name)):
+                tensor_fields = ["text_field_1", "text_field_2", "text_field_3"] if "unstr" \
+                                                                                    in test_index_name else None
+                d1 = {
+                    "text_field_1": "Cool Document 1",
+                    "text_field_2": "some extra info",
+                    "_id": "e197e580-0393-4f4e-90e9-8cdf4b17e339"
+                }
+                d2 = {
+                    "text_field_1": "Just Your Average Doc",
+                    "text_field_2": "this is a solid doc",
+                    "_id": "123456"
+                }
+                res = self.client.index(test_index_name).add_documents(
+                    [
+                        d1, d2
+                    ], tensor_fields=tensor_fields
+                )
+
+                get_response = self.client.index(test_index_name).get_documents_by_ids(
+                    document_ids=["e197e580-0393-4f4e-90e9-8cdf4b17e339", "123456", "not_exist"], expose_facets=True
+                )
+
+                post_response = self.get_documents_by_ids_via_get(
+                    index_name=test_index_name,
+                    document_ids=["e197e580-0393-4f4e-90e9-8cdf4b17e339", "123456", "not_exist"], expose_facets=True
+                ).json()
+
+                self.assertEqual(
+                    get_response,
+                    post_response
+                )
