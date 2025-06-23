8c6c140bbfdf5ebc31d439c4c8e939ed32448591
Li Wan <li@marqo.ai>
2025-06-23 19:54:36 +1000

    Add sort-by parameters

--- src/marqo/index.py
+++ src/marqo/index.py
@@ -225,7 +225,8 @@ class Index:
                rerank_depth: Optional[int] = None, facets: Optional[dict] = None,
                track_total_hits: Optional[bool] = None,
                approximate_threshold: Optional[float] = None,
-               sort_by: Optional[dict] = None
+               sort_by: Optional[dict] = None,
+               relevance_cutoff: Optional[dict] = None
                ) -> Dict[str, Any]:
         """Search the index.
 
@@ -269,7 +270,8 @@ class Index:
             track_total_hits: return total number of lexical matches
             approximate_threshold: hit ratio threshold for deciding if a nearest neighbor search should be performed as
                 an exact search, rather than an approximate search
-            sort_by: a dictionary of the sort_by parameters to be used for sorting the results.
+            sort_by: a dictionary of the sort_by parameters to be used for sorting the results
+            relevance_cutoff: a dictionary of the relevance cutoff parameters
 
         Returns:
             Dictionary with hits and other metadata
@@ -309,7 +311,8 @@ class Index:
             "hybridParameters": hybrid_parameters,
             "facets": facets,
             "trackTotalHits": track_total_hits,
-            "sortBy": sort_by
+            "sortBy": sort_by,
+            "relevanceCutoff": relevance_cutoff
         }
 
         body = {k: v for k, v in body.items() if v is not None}
