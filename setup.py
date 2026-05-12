28798eb2d81558113d1fae4e1eaafc1eedde7377
pandu-k <107458762+pandu-k@users.noreply.github.com>
2026-05-13 09:16:49 +1000

    bump version; add keyword

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ setup(
         "tox"
     ],
     name="marqo",
-    version="3.18.1",
+    version="3.18.2",
     author="marqo org",
     author_email="org@marqo.io",
     description="AI-native ecommerce search platform with semantic search and personalization for fashion, beauty, electronics, and home goods.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(where="src", exclude=("tests*",)),
-    keywords="search python marqo tensor neural hybrid semantic vector embedding ecommerce personalization",
+    keywords="search python marqo tensor neural hybrid semantic vector embedding ecommerce personalization merchandizing",
     platform="any",
     classifiers=[
         "Programming Language :: Python :: 3.8",
